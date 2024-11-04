48400e0b5bdbc5a73aad37b7330e186584f4f00f
Danny Nguyen <nguyenduongpa@gmail.com>
2024-11-04 08:01:35 -0800

    d

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -2,9 +2,11 @@ import { formatDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
+import Image from "next/image";
+import { Button } from './ui/button';
 
 const StartupCard = ({post}: {post: StartupTypeCard}) => {
-    const { _createdAt, views, author: {_id: authorId, name}, title, category, _id, image} = post;
+    const { _createdAt, views, author: {_id: authorId, name}, title, category, _id, image, description} = post;
     return (
         <li className="startup-card group">
             <div className="flex-between">
@@ -24,10 +26,36 @@ const StartupCard = ({post}: {post: StartupTypeCard}) => {
                             {name}
                         </p>
                     </Link>
-                    <Link>
-                    
+                    <Link href={`/startup/${_id}`}>
+                        <h3 className="text-26-semi-bold line-clamp-1">
+                            {title}
+                        </h3>
                     </Link>
                 </div>
+
+                <Link href={`/user/${authorId}`}>
+                    <Image src="https://placehold.co/600x400" alt="placeholder" width={48} height={48} className="rounded-full" />
+                </Link>
+            </div>
+
+            <Link href={`/startup/${_id}`}>
+                <p className="startup-card_desc">
+                    {description}
+                </p>
+
+                <img src={image} alt="placeholder" className="startup-card_img" />
+            </Link>
+
+            <div className="flex-between gap-3 mt-5">
+                <Link href={`/?query=${category.toLowerCase()}`}>
+                    <p className="text-16-medium">{category}</p>
+                </Link>
+
+                <Button className="startup-card_btn" asChild>
+                    <Link href={`/startup/${_id}`}>
+                        Details
+                    </Link>
+                </Button>
             </div>
         </li>
     )
