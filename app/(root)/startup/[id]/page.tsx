db60987345b3a496e74f298d3fc804ecabc5569d
Danny Nguyen <nguyenduongpa@gmail.com>
2024-11-12 07:52:40 -0800

    d

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -25,45 +25,48 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
         <>
             <section className="pink_container !min-h-[230px]">
                 <p className="tag">{formatDate(post?._createdAt)}</p>
+
                 <h1 className="heading">{post.title}</h1>
                 <p className="sub-heading !max-w-5xl">{post.description}</p>
             </section>
 
             <section className="section_container">
                 <img src={post.image} alt="thumbnail" className="w-full h-auto rounded-xl"/>
+
                 <div className="space-y-5 mt-10 max-w-4xl mx-auto">
                     <div className="flex-between gap-5">
                         <Link href={`/user/${post.author?._id}`} className="flex gap-2 items-center mb-3">
-                            <Image src={post.author.image} alt="avatar" width={64} height={64} className="rounded-full drop-shadow-lg"/>
-                            <div>
-                                <p className="text-20-medium">{post.author.name}</p>
-                                <p className="text-16-medium !text-black-300">@{post.author.username}</p>
-                            </div>
+                        <Image src={post.author.image} alt="avatar" width={64} height={64} className="rounded-full drop-shadow-lg" />
+
+                        <div>
+                            <p className="text-20-medium">{post.author.name}</p>
+                            <p className="text-16-medium !text-black-300">
+                            @{post.author.username}
+                            </p>
+                        </div>
                         </Link>
 
-                        <p className="category-tag">
-                            {post.category}
-                        </p>
-
-                        <h3 className="text-30-bold">
-                            Pitch Details
-                        </h3>
-                        {parsedContent ? (
-                            <article className="prose max-w-4xl font-work-sans break-all" dangerouslySetInnerHTML={{ __html: parsedContent }} />
-                        ): (
-                            <p className="no-result">
-                                No details provided
-                            </p>
-                        )}
+                        <p className="category-tag">{post.category}</p>
                     </div>
 
-                    <hr className="divider"/>
+                    <h3 className="text-30-bold">Pitch Details</h3>
+                    {parsedContent ? (
+                        <article
+                        className="prose max-w-4xl font-work-sans break-all"
+                        dangerouslySetInnerHTML={{ __html: parsedContent }}
+                        />
+                    ) : (
+                        <p className="no-result">No details provided</p>
+                    )}
                 </div>
+
+                <hr className="divider" />
+
+
+                <Suspense fallback={<Skeleton className="view_skeleton" />}>
+                    <View id={id} />
+                </Suspense>
             </section>
-            
-            <Suspense fallback={<Skeleton className="view_skeleton"/>}>
-                <View id={id}/>
-            </Suspense>
         </>
     )
 }
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -30,7 +30,7 @@ export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type=="startup" && _id == $id
 }`)
 
 export const STARTUP_VIEWS_QUERY = defineQuery(`
-    *[_type=="startup" && _id == $id[0] {
+    *[_type == "startup" && _id == $id][0]{
         _id, views
-    } 
-]`)
+    }
+`);
